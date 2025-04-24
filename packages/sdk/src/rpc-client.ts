f5b559f25b029f8b324245008b2a8aadc463ec32
Paul Chen <p22626262@gmail.com>
2025-04-24 19:15:36 +0800

    refactor: improve comments in watchBalanceChanged function

--- packages/sdk/src/rpc-client.ts
+++ packages/sdk/src/rpc-client.ts
@@ -24,6 +24,7 @@ export const connectToRPC = async () => {
 export const watchBalanceChanged = (address: string) => {
   if (!rpcClient) throw new Error("RPC client is not connected");
 
+  // Emit the initial balance when the account is changed
   rpcClient.getBalanceByAddress({ address }).then((balanceResponse) => {
     window.postMessage({
       id: "kas:balance_changed",
@@ -31,11 +32,11 @@ export const watchBalanceChanged = (address: string) => {
     });
   });
 
+  // Remove existing event listeners as it only allows one listener at a time
   rpcClient.removeEventListener("utxos-changed");
   rpcClient.subscribeUtxosChanged([address]);
   rpcClient.addEventListener("utxos-changed", async (event) => {
     const balanceResponse = await rpcClient?.getBalanceByAddress({ address });
-
     window.postMessage({
       id: "kas:balance_changed",
       response: Number(balanceResponse?.balance ?? 0),
